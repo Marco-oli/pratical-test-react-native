bc693f4aff61f52775690e46f0fe64c9b74b5e37
Marco Oliveira <marco.aurelio@nomadglobal.com>
2022-08-23 20:37:27 -0300

    style: insert colors and add images in project

---
+++ Sementes/src/assets/colors.ts
@@ -0,0 +1,5 @@
+export const colors = {
+  white: '#ffffff',
+  main: '#333333',
+  gray6: '#F2F2F2',
+};
--- Sementes/src/assets/images/index.ts
+++ Sementes/src/assets/images/index.ts
@@ -3,4 +3,6 @@ export const images = {
   bookIcon: require('./bookIcon.png'),
   heartIcon: require('./heart.png'),
   micIcon: require('./micIcon.png'),
+  perfilImage: require('./perfil.png'),
+  searchIcon: require('./search.png'),
 };
