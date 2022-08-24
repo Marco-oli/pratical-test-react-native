951edc60fb5897d4186c63e7dcb50fbec981fcb5
Marco Oliveira <marco.aurelio@nomadglobal.com>
2022-08-23 22:16:43 -0300

    feat: create new call hook api

---
+++ Sementes/src/hooks/Home/useGetCollections.ts
@@ -0,0 +1,33 @@
+import {useState} from 'react';
+import {BookProps} from '../../interfaces/bookInterface';
+import {api, api_key} from '../../services/api';
+
+export const useGetCollections = () => {
+  const [collections, setCollections] = useState<BookProps[]>();
+  const [collectionsError, setCollectionsError] = useState(false);
+  const [collectionsLoading, setCollectionsLoading] = useState(false);
+
+  const getCollections = async () => {
+    try {
+      setCollectionsLoading(true);
+      setCollectionsError(false);
+
+      const reponse = await api.get(
+        `/svc/books/v3/lists/overview.json${api_key}`,
+      );
+
+      setCollections(reponse.data.results.lists[0].books);
+    } catch (err) {
+      setCollectionsError(true);
+    } finally {
+      setCollectionsLoading(false);
+    }
+  };
+
+  return {
+    collections,
+    collectionsError,
+    collectionsLoading,
+    getCollections,
+  };
+};
