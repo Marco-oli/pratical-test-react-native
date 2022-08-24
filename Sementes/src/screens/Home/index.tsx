59b0ca0d3aa305fca1dee90fa7cacec61b3a672d
Marco Oliveira <marco.aurelio@nomadglobal.com>
2022-08-23 22:41:50 -0300

    feat: finish Home screen

--- Sementes/src/screens/Home/index.tsx
+++ Sementes/src/screens/Home/index.tsx
@@ -1,24 +1,64 @@
-import React, {useEffect, useState} from 'react';
-import {Image, StyleSheet, Text, TextInput, View} from 'react-native';
+import {NavigationProp, useNavigation} from '@react-navigation/native';
+import React, {useCallback, useEffect, useState} from 'react';
+import {
+  ActivityIndicator,
+  FlatList,
+  Image,
+  ScrollView,
+  StyleSheet,
+  Text,
+  TextInput,
+  View,
+} from 'react-native';
 import {colors} from '../../assets/colors';
 import {images} from '../../assets/images';
+import {CategoryCard} from '../../components/CategoryCard';
+import {MainCard} from '../../components/MainCard';
 import {useGetCategory} from '../../hooks/Home/useGetCategory';
+import {useGetCollections} from '../../hooks/Home/useGetCollections';
+import {CategoryProps} from '../../interfaces/mainInterface';
+import {HomeStackProps} from '../../routes/HomeStack';
+import {randomColor} from '../../utils/randonBgColor';
 import {Header} from './components/Header';
 
 export const Home = () => {
-  const {categories, categoriesError, categoriesLoading, getCategories} =
-    useGetCategory();
+  const {categories, categoriesLoading, getCategories} = useGetCategory();
+  const {collections, collectionsLoading, getCollections} = useGetCollections();
+  const navigation =
+    useNavigation<NavigationProp<HomeStackProps, 'Category'>>();
 
   const [text, setText] = useState('');
 
   useEffect(() => {
     getCategories();
+    getCollections();
   }, []);
+
+  const filteredCollections = useCallback(() => {
+    if (text !== '') {
+      return collections?.filter(
+        item =>
+          item.author.toLowerCase().includes(text.toLowerCase()) ||
+          item.title.toLowerCase().includes(text.toLowerCase()),
+      );
+    }
+
+    return collections;
+  }, [collections, text]);
+
+  const handleNavigateToCategory = useCallback(
+    (category: CategoryProps) => {
+      navigation.navigate('Category', {
+        category,
+      });
+    },
+    [navigation],
+  );
 
   console.log(categories);
 
   return (
-    <View style={styles.container}>
+    <ScrollView style={styles.container}>
       <View style={styles.containerHeader}>
         <Header />
       </View>
@@ -29,17 +69,75 @@ export const Home = () => {
           onChangeText={setText}
           style={styles.input}
           placeholder="Qual livro você gostaria de ler hoje?"
+          editable={!categoriesLoading || !collectionsLoading}
         />
-
         <Image source={images.searchIcon} style={styles.searchIcon} />
       </View>
-    </View>
+
+      {categoriesLoading || collectionsLoading ? (
+        <View>
+          <ActivityIndicator size="large" color={colors.main} />
+        </View>
+      ) : (
+        <>
+          <View style={styles.containerList}>
+            <Text style={styles.titleList}>Para você</Text>
+            <FlatList
+              horizontal
+              showsHorizontalScrollIndicator={false}
+              data={filteredCollections()}
+              renderItem={({item}) => (
+                <MainCard
+                  book_image={item.book_image}
+                  author={item.author}
+                  title={item.title}
+                  onPress={() => {}}
+                />
+              )}
+            />
+          </View>
+
+          <View style={styles.containerList}>
+            <Text style={styles.titleList}>Categorias</Text>
+            <FlatList
+              horizontal
+              showsHorizontalScrollIndicator={false}
+              data={categories}
+              renderItem={({item}) => (
+                <CategoryCard
+                  title={item.list_name}
+                  color={randomColor()}
+                  onPress={() => handleNavigateToCategory(item)}
+                />
+              )}
+            />
+          </View>
+
+          <View style={styles.containerList}>
+            <Text style={styles.titleList}>Os mais lidos da semana</Text>
+            <FlatList
+              horizontal
+              showsHorizontalScrollIndicator={false}
+              data={filteredCollections()}
+              renderItem={({item}) => (
+                <MainCard
+                  book_image={item.book_image}
+                  author={item.author}
+                  title={item.title}
+                  onPress={() => {}}
+                />
+              )}
+            />
+          </View>
+        </>
+      )}
+    </ScrollView>
   );
 };
 
 const styles = StyleSheet.create({
   container: {
-    flex: 1,
+    flexGrow: 1,
     backgroundColor: colors.white,
     paddingVertical: 24,
   },
@@ -64,5 +162,16 @@ const styles = StyleSheet.create({
   searchIcon: {
     position: 'absolute',
     right: 16,
+  },
+  containerList: {
+    marginLeft: 16,
+    marginBottom: 32,
+  },
+  titleList: {
+    fontSize: 18,
+    lineHeight: 22,
+    fontWeight: '900',
+    color: colors.main,
+    marginBottom: 16,
   },
 });
